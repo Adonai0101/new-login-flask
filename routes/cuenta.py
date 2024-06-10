91b188b1c2ec3795d1ebebf1073c3bb01d16b717
pampire <pampire1995@gmail.com>
2024-06-10 01:26:34 -0600

    nueva forma de enviar los formularios

--- routes/cuenta.py
+++ routes/cuenta.py
@@ -10,34 +10,36 @@ from db import mongo
 
 cuenta = Blueprint('cuenta',__name__)
 
-@cuenta.route('/', methods = ['GET','POST'])
+@cuenta.route('/', methods = ['GET'])
 @login_required
-def index():
-    if request.method == 'POST':
-        user_session = session.get("user")
-        uid = user_session['uid']
-        user = {
-            'nombre': request.form['nombre'],
-            'telefono': request.form['telefono']
-        }
-        #Validando los datos
-        if is_nombre(user['nombre']) and is_telefono(user['telefono']):
-        
-            resultado = mongo.db.user.update_one({'uid':uid},{"$set":user}) 
-            #modificando la informacion del qr segun el usuario
-            resultado = mongo.db.qr.update_one({'uid':uid},{"$set":{
-                'user.nombre':user['nombre'],
-                'user.telefono':user['telefono'],
-                }})
-            
-            get_user()
-            flash('¡Usuario modificado con éxito!', 'success')
-        else:
-            flash('¡Error al ingresar los datos!', 'error')
-            
+def index(): 
     return render_template('usuario/perfil.html',user = session.get('user'))
 
+@cuenta.route('/',methods = ['POST'])
+def cuenta_post():
+    user_session = session.get("user")
+    uid = user_session['uid']
+    user = {
+        'nombre': request.json['nombre'],
+        'telefono': request.json['telefono']
+    }
+    print(user)
+    #Validando los datos
+    if is_nombre(user['nombre']) and is_telefono(user['telefono']):
+    
+        resultado = mongo.db.user.update_one({'uid':uid},{"$set":user}) 
+        #modificando la informacion del qr segun el usuario
+        resultado = mongo.db.qr.update_one({'uid':uid},{"$set":{
+            'user.nombre':user['nombre'],
+            'user.telefono':user['telefono'],
+            }})
+        
+        get_user()
+        flash('¡Usuario modificado con éxito!', 'success')
+    else:
+        flash('¡Error al ingresar los datos!', 'error')
 
+    return jsonify({'msj':'usuario modificada con exito'}),200
 
 @cuenta.route('/updatefoto',methods = ['POST'])
 def update_foto():
